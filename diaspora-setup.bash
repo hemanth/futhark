cee790aa7bb44b386dc0c27c70563fec920a4b4c
hemanth.hm <hemanth.hm@gmail.com>
2010-09-16 15:25:14 +0530

    Added rake install and replaced apt-get with aptitude

--- diaspora-setup.bash
+++ diaspora-setup.bash
@@ -11,14 +11,19 @@ shopt -s extglob
 [ "$(whoami)" != "root" ] && echo "Please run this script as root/sudo" && exit 1
 
 # Install build tools 
-echo "Installing build tools..."
-sudo apt-get install build-essential libxslt1.1 libxslt1-dev libxml2
+echo "Installing build tools.."
+sudo aptitude install build-essential libxslt1.1 libxslt1-dev libxml2
 echo "..Done installing build tools"
 
 # Install Ruby 1.8.7 
-echo "Installing ruby-full Ruby 1.8.7" 
-sudo apt-get install ruby-full
+echo "Installing ruby-full Ruby 1.8.7.." 
+sudo aptitude install ruby-full
 echo "..Done installing Ruby"
+
+# Install Rake 
+echo "Installing rake.."
+sudo aptitude install rake
+echo "..Done installing rake"
 
 # Get the current release and install mongodb
 lsb=$(lsb_release -rs)
@@ -31,19 +36,19 @@ echo "."
 echo "Fetching keys.."
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 echo "."
-sudo apt-get update
+sudo aptitude update
 echo "."
-sudo apt-get install mongodb-stable
+sudo aptitude install mongodb-stable
 echo "Done installing monngodb-stable.."
 
 # Install imagemagick
 echo "Installing imagemagick.."
-sudo apt-get install imagemagick libmagick9-dev
+sudo aptitude install imagemagick libmagick9-dev
 echo "Installed imagemagick.."
 
 # Install git-core
 echo "Installing git-core.."
-sudo apt-get install git-core
+sudo aptitude install git-core
 echo "Installed git-core.."
 
 # Setting up ruby gems
