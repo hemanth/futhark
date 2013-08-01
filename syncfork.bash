d80c5ac13b654a38992f466435fccd17597801ae
Hemanth.HM <hemanth.hm@gmail.com>
2013-08-01 21:13:31 +0530

    head must have been tail

--- syncfork.bash
+++ syncfork.bash
@@ -14,7 +14,7 @@ part=${url#*github.com*/}
 
 # ZOMG parse JSON using the github api get the parent git_url.
 # would suggest to use curl -s .. | jq -r '.parent.git_url'  
-upstream=$(curl -s https://api.github.com/repos/${part%.git} | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep '"git_url":' | sed 's/:/ /1' | awk -F" " '{ print $2 }' | uniq | head -1)
+upstream=$(curl -s https://api.github.com/repos/${part%.git} | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep '"git_url":' | sed 's/:/ /1' | awk -F" " '{ print $2 }' | uniq | tail -1)
 
 # Rest of it...
 git remote add upstream $upstream
