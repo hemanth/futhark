419939caf6cf66c913e42b7057677747e3e2aaff
hemanth.hm <hemanth.hm@gmail.com>
2014-02-26 11:46:35 +0530

    Clean up.

--- notify.js
+++ notify.js
@@ -1,16 +1,18 @@
-function notify(text) { 
-span=document.body.appendChild(document.createElement('span')); 
-span.innerHTML=text;
-span.style.cssText="z-index:99;font-size:18px;background:#FFF1A8;top:0";
-span.style.position='fixed';
-span.style.left=((document.body.clientWidth - span.clientWidth) / 2)+"px"; 
-window.setTimeout(function(){document.body.removeChild(span)}, 3000)
+function notify(text) {
+    span=document.body.appendChild(document.createElement('span'));
+    span.innerHTML=text;
+    span.style.cssText="z-index:99;font-size:18px;background:#FFF1A8;top:0";
+    span.style.position='fixed';
+    span.style.left=((document.body.clientWidth - span.clientWidth) / 2)+"px";
+    window.setTimeout(function(){
+      document.body.removeChild(span);
+    }, 3000);
 }
  
-window.addEventListener('online',function(evt) { 
-    notify("And....we are back!"); 
+window.addEventListener('online',function(evt) {
+    notify("And....we are back!");
 });
  
-window.addEventListener('offline',function(evt) { 
-    notify("OOPS....you are offline!"); 
+window.addEventListener('offline',function(evt) {
+    notify("OOPS....you are offline!");
 });
--- plot.sh
+++ plot.sh
@@ -4,26 +4,26 @@
 
 # Usage
 show_help(){
-echo "Usage is $0 a|m|n|c|h"
-echo "-a or --all to plot cpu(c),mem(m) and net(n)"
+	echo "Usage is $0 a|m|n|c|h"
+	echo "-a or --all to plot cpu(c),mem(m) and net(n)"
 }
 
 # Make directory to store the results
 setdir(){
-mkdir -p Stats
-cd Stats
+	mkdir -p Stats
+	cd Stats
 }
 
 # Use dstat to get the data set
 gendata(){
-echo "Collecting stats for 60sec with an interval of 10sec"
-dstat -tmnc 10 > dstat.dat&
-[ "$?" -ne 0 ] && echo "Please check if you have installed dstat" && exit 1
-sleep 60s
-exec 2>/dev/null
-kill $! >/dev/null 2>&1
-#Remove the headers
-sed '1,2d' dstat.dat > stat.dat
+	echo "Collecting stats for 60sec with an interval of 10sec"
+	dstat -tmnc 10 > dstat.dat&
+	[ "$?" -ne 0 ] && echo "Please check if you have installed dstat" && exit 1
+	sleep 60s
+	exec 2>/dev/null
+	kill $! >/dev/null 2>&1
+	#Remove the headers
+	sed '1,2d' dstat.dat > stat.dat
 }
 
 # Use GNU plot to plot the graph
@@ -43,52 +43,52 @@ EOF
 
 # Plot CPU usage 
 plotcpu(){
-fileType="png"
-output='"cpu.png"'
-title='"cpu-usage"'
-xlabel='"time"'
-ylabel='"percent"'
+	fileType="png"
+	output='"cpu.png"'
+	title='"cpu-usage"'
+	xlabel='"time"'
+	ylabel='"percent"'
 
-# Using an arry presrving the '"quotes"' is very much nessary 
-plot=( '"stat.dat"' using 1:3 title '"system"' with lines,'"stat.dat"' using 1:2 title '"user"' with lines,'"stat.dat"' using 1:4 title '"idle"' with lines )
+	# Using an arry presrving the '"quotes"' is very much nessary 
+	plot=( '"stat.dat"' using 1:3 title '"system"' with lines,'"stat.dat"' using 1:2 title '"user"' with lines,'"stat.dat"' using 1:4 title '"idle"' with lines )
 
-graph
+	graph
 
 }
 
 # Plot memory usage
 plotmem(){
-fileType="png"
-output='"memory.png"'
-title='"memory-usage"'
-xlabel='"time"'
-ylabel='"size(Mb)"'
+	fileType="png"
+	output='"memory.png"'
+	title='"memory-usage"'
+	xlabel='"time"'
+	ylabel='"size(Mb)"'
 
-plot=( '"stat.dat"' using 1:8 title '"used"' with lines,'"stat.dat"' using 1:9 title '"buff"' with lines, '"stat.dat"' using 1:10 title '"cach"' with lines,'"stat.dat"' using 1:11 title '"free"' with lines )
+	plot=( '"stat.dat"' using 1:8 title '"used"' with lines,'"stat.dat"' using 1:9 title '"buff"' with lines, '"stat.dat"' using 1:10 title '"cach"' with lines,'"stat.dat"' using 1:11 title '"free"' with lines )
 
-graph "png" '"memo.png"' '"cpu-usage"' '"time"' '"percent"' $plot
+	graph "png" '"memo.png"' '"cpu-usage"' '"time"' '"percent"' $plot
 }
 
 # Plot network usage
 plotnet(){
-fileType="png"
-output='"network.png"'
-title='"network-usage"'
-xlabel='"time"'
-ylabel='"size(k)"'
+	fileType="png"
+	output='"network.png"'
+	title='"network-usage"'
+	xlabel='"time"'
+	ylabel='"size(k)"'
 
-plot=( '"stat.dat"' using 1:11 title '"sent"' with lines,'"stat.dat"' using 1:12 title '"recvd"' with lines )
+	plot=( '"stat.dat"' using 1:11 title '"sent"' with lines,'"stat.dat"' using 1:12 title '"recvd"' with lines )
 
-graph 
+	graph 
 
 }
 
 # Clean up all the collected stats 
 clean(){
-echo "Cleaning"
-cd Stats
-rm -r *.dat
-echo "Done!"
+	echo "Cleaning"
+	cd Stats
+	rm -r *.dat
+	echo "Done!"
 }
 
 #Check for the first input if it's zero show help
