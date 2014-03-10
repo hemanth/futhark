9a9786d2b27c3176e5f5b5b704479f8a62f33b18
hemanth.hm <hemanth.hm@gmail.com>
2014-03-10 14:18:19 +0530

    Clean up.

--- getmevido.bash
+++ getmevido.bash
@@ -1,19 +1,19 @@
 cat >> ~/.bashrc << "EOF"
 getme(){
-if [[ $1 == avi && $2 != "" ]]; then
-url=$2
-filename=$(echo $url | cut -d"=" -f2)
-youtube-dl $url 
-ffmpeg -i $filename.flv $filename.avi
-else
-if [[ $1 == ogg && $2 != "" ]]; then
-url=$2
-filename=$(echo $url | cut -d"=" -f2)
-youtube-dl $url
-ffmpeg2theora -v 10 -S 0 -a 10 --sync $filename
-else
-echo "Use getme <ogg/avi> <link>"
-fi
-fi 
+	if [[ $1 == avi && $2 != "" ]]; then
+		url=$2
+		filename=$(echo $url | cut -d"=" -f2)
+		youtube-dl $url 
+		ffmpeg -i $filename.flv $filename.avi
+	else
+		if [[ $1 == ogg && $2 != "" ]]; then
+			url=$2
+			filename=$(echo $url | cut -d"=" -f2)
+			youtube-dl $url
+			ffmpeg2theora -v 10 -S 0 -a 10 --sync $filename
+		else
+		  echo "Use getme <ogg/avi> <link>"
+		fi
+	fi 
 }
 EOF
